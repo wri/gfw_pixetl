561f867f6c173f8b64d0ef8df8052e9e48ee5984
Thomas Maschler <tmaschler@wri.org>
2020-01-17 14:25:45 -0500

    add more tests, reduce number of instance families for compute environment
    Signed-off-by: Thomas Maschler <tmaschler@wri.org>

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,21 +14,22 @@ provider "aws" {
 }
 
 module "container_registry" {
-  source      = "git::https://github.com/wri/gfw-terraform-modules.git//modules/container_registry?ref=v0.0.1"
-  image_name  = local.project
-  root_dir    = "../${path.root}"
+  source     = "git::https://github.com/wri/gfw-terraform-modules.git//modules/container_registry?ref=v0.0.1"
+  image_name = local.project
+  root_dir   = "../${path.root}"
 }
 
 
 
 module "compute_environment_ephemeral_storage" {
-  source             = "git::https://github.com/wri/gfw-terraform-modules.git//modules/compute_environment_ephemeral_storage?ref=v0.0.1"
+  source             = "git::https://github.com/wri/gfw-terraform-modules.git//modules/compute_environment_ephemeral_storage?ref=v0.0.2"
   project            = local.project
   key_pair           = data.terraform_remote_state.core.outputs.key_pair_tmaschler_gfw
   subnets            = data.terraform_remote_state.core.outputs.private_subnet_ids
   tags               = local.tags
   security_group_ids = [data.terraform_remote_state.core.outputs.default_security_group_id]
   iam_policy_arn     = [data.terraform_remote_state.core.outputs.iam_policy_s3_write_data-lake_arn]
-        suffix = local.name_suffix
+  suffix             = local.name_suffix
+  instance_types     = ["r5d"]
 
 }
