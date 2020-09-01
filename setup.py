e701f892b0e4014a683e7748c02eed4a69441d10
dagibbs22 <dagibbs22@gmail.com>
2020-09-01 12:31:12 -0400

    Preparing tiles for model v1.2.0 standard run.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     packages=["gfw_tile_prep"],
     author="Thomas Maschler",
     license="MIT",
-    install_requires=["parallelpipe", "psycopg2-binary", "rasterio", "boto3<1.13"],
+    install_requires=["parallelpipe", "psycopg2-binary", "numpy<1.18", "rasterio", "boto3<1.13"],
     # Need boto3<1.13 because more recent versions require a more recent version of numpy which requires a more recent of Python3
     scripts=["gfw_tile_prep/prep_tiles.py"],
 )
