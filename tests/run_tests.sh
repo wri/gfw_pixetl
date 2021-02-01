c504470b239414d41201d194fa1bdcc0df27bada
Thomas Maschler <tmaschler@wri.org>
2021-02-01 10:59:25 -0500

    Don't fetch secret when no secret ARN is set.
    Signed-off-by: Thomas Maschler <tmaschler@wri.org>

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -2,5 +2,5 @@
 
 set -e
 
-python /usr/local/app/tests/startup.py
+#python /usr/local/app/tests/startup.py
 /usr/local/app/wait_for_postgres.sh pytest -vv --cov-report term --cov-report xml:/usr/local/app/tests/cobertura.xml --cov=gfw_pixetl "$@"
