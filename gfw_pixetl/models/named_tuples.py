bf8adbe4d868b3cf2126fc708aa56bbde7ff470e
Daniel Mannarino <daniel.mannarino@gmail.com>
2023-05-30 16:46:27 -0400

    Remove dead code, workaround for old proj package from grid code

--- gfw_pixetl/models/named_tuples.py
+++ gfw_pixetl/models/named_tuples.py
@@ -3,32 +3,6 @@ from typing import NamedTuple, Optional
 from shapely.geometry import Polygon
 
 
-class AreaOfUse(NamedTuple):
-    """Area Of Use for projections.
-
-    Copied from pyproj.aoi.AreaOfUse version 3.0 PyProj Version 2.6 does
-    not expose this class.
-    """
-
-    #: West bound of area of use.
-    west: float
-    #: South bound of area of use.
-    south: float
-    #: East bound of area of use.
-    east: float
-    #: North bound of area of use.
-    north: float
-    #: Name of area of use.
-    name: Optional[str] = None
-
-    @property
-    def bounds(self):
-        return self.west, self.south, self.east, self.north
-
-    def __str__(self):
-        return f"- name: {self.name}\n" f"- bounds: {self.bounds}"
-
-
 class InputBandElement(NamedTuple):
 
     geometry: Optional[Polygon]
