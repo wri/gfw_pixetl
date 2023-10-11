a7e0fe60f1aa5cb1a4e8b747b613d44a1cc98c15
Daniel Mannarino <daniel.mannarino@gmail.com>
2023-10-11 15:01:33 -0400

    Remove some noisy logging

--- gfw_pixetl/tiles/tile.py
+++ gfw_pixetl/tiles/tile.py
@@ -43,7 +43,6 @@ class Tile(ABC):
         return self.tile_id == other.tile_id and self.grid == other.grid
 
     def __init__(self, tile_id: str, grid: Grid, layer: Layer) -> None:
-
         self.grid: Grid = grid
         self.layer: Layer = layer
 
@@ -73,16 +72,12 @@ class Tile(ABC):
 
         gdal_profile.update(self.layer.dst_profile)
 
-        LOGGER.debug(f"GDAL Profile for tile {self.tile_id}: {gdal_profile}")
-
         # Drop GDAL specific optimizations which might not be readable by other applications
         geotiff_profile = copy.deepcopy(gdal_profile)
         geotiff_profile.pop("nbits", None)
         geotiff_profile.pop("sparse_ok", None)
         geotiff_profile.pop("interleave", None)
         geotiff_profile["compress"] = "DEFLATE"
-
-        LOGGER.debug(f"GEOTIFF Profile for tile {self.tile_id}: {geotiff_profile}")
 
         self.dst: Dict[str, Destination] = {
             DstFormat.gdal_geotiff: Destination(
@@ -121,7 +116,6 @@ class Tile(ABC):
         self.local_dst[dst_format] = RasterSource(uri)
 
     def get_local_dst_uri(self, dst_format) -> str:
-
         prefix = f"{self.work_dir}/{dst_format}"
         LOGGER.debug(f"Attempt to create local folder {prefix} if not already exists")
         os.makedirs(f"{prefix}", exist_ok=True)
