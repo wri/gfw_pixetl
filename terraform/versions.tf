dd63e66a21fd8c96bf2076a83b7a25cd1ea01bba
Daniel Mannarino <daniel.mannarino@gmail.com>
2026-02-23 05:00:54 -0500

    Uppgrade AWS prov. versions

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 2.45.0, < 4.0"
+      version = ">= 4, < 5"
     }
     local = {
       source = "hashicorp/local"
